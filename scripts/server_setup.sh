b858d683fd37dd601bc9e7f5a972536cfb116f7d
Akihiro Nakao <an@hymena.jp>
2013-09-22 19:27:08 +0900

    add bc

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -3,5 +3,5 @@
 # CentOS 6 64bit
 rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
-yum --enablerepo=remi install php php-mbstring php-pear php-pecl-memcache php-pdo php-mysql php-mssql php-pgsql php-pecl-apc httpd-devel mysql-server vim-enhanced screen zsh memcached php-soap php-mcrypt php-gd php-xml php-dba telnet ntp bind-utils unzip rsync postfix mod_ssl git cronolog php-tidy mlocate mailx jwhois ImageMagick php-pecl-imagick s3cmd perl-CPAN wget fio
+yum --enablerepo=remi install php php-mbstring php-pear php-pecl-memcache php-pdo php-mysql php-mssql php-pgsql php-pecl-apc httpd-devel mysql-server vim-enhanced screen zsh memcached php-soap php-mcrypt php-gd php-xml php-dba telnet ntp bind-utils unzip rsync postfix mod_ssl git cronolog php-tidy mlocate mailx jwhois ImageMagick php-pecl-imagick s3cmd perl-CPAN wget fio bc
 
