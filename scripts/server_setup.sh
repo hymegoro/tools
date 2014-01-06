32c3d699fbd63970ba818a8a4157e3dbd8eae19b
Akihiro Nakao <an@hymena.jp>
2014-01-06 15:15:47 +0900

    add zip

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -4,5 +4,4 @@
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 
-yum --enablerepo=remi install gcc gcc-c++ php php-devel php-mbstring php-pear php-pecl-memcache php-pdo php-mysql php-mssql php-pgsql php-pecl-apc httpd-devel mysql-server vim-enhanced screen zsh memcached php-soap php-mcrypt php-gd php-xml php-dba telnet ntp bind-utils unzip rsync postfix mod_ssl git cronolog php-tidy mlocate mailx jwhois ImageMagick php-pecl-imagick s3cmd perl-CPAN wget fio bc
-
+yum --enablerepo=remi install gcc gcc-c++ php php-devel php-mbstring php-pear php-pecl-memcache php-pdo php-mysql php-mssql php-pgsql php-pecl-apc httpd-devel mysql-server vim-enhanced screen zsh memcached php-soap php-mcrypt php-gd php-xml php-dba telnet ntp bind-utils unzip rsync postfix mod_ssl git cronolog php-tidy mlocate mailx jwhois ImageMagick php-pecl-imagick s3cmd perl-CPAN wget fio bc ack openssl openssl-devel zlib-devel readline-devel poppler-utils zip
