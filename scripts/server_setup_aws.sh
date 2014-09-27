e700f167a3ca0f77b160fb659729bdaf6532cae0
Akihiro Nakao <an@hymena.jp>
2014-09-27 17:44:25 +0900

    add server setup aws

---
+++ scripts/server_setup_aws.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+yum install gcc gcc-c++ mysql-server vim-enhanced screen zsh memcached  telnet ntp bind-utils unzip rsync postfix mod_ssl git cronolog mlocate mailx jwhois ImageMagick php-pecl-imagick s3cmd perl-CPAN wget fio bc ack openssl openssl-devel zlib-devel readline-devel poppler-utils zip nkf rubygems ruby-devel nodejs npm lynx
+
+yum install httpd24 httpd24-devel php55 php55-devel php55-mbstring php55-pear php55-pecl-memcache php55-memcached php55-pdo php55-mysql php55-mssql php55-pgsql php55-soap php55-mcrypt php55-gd php55-xml php55-dba php55-tidy
